a359094ed3fbe740890e07245694e5fb605e4803
Scooby202 <erickscooby2021@gmail.com>
2026-04-13 16:16:43 -0300

    Classe A e B implementadas

---
+++ A.java
@@ -0,0 +1,52 @@
+public class A {
+
+    private int A1;
+    private float A2;
+
+    public A () {
+
+        A1 = 0;
+        A2 = 0;
+
+    }
+
+    public int getA1 () {
+
+        return A1;
+
+    }
+
+    public float getA2 () {
+
+        return A2;
+
+    }
+
+    public void setA1 (int mudarValor) {
+
+        A1 = mudarValor;
+
+    }
+
+    public void setA2 (float mudarValor) {
+
+        A2 = mudarValor;
+
+    }
+
+    public void MA1 () {
+
+        System.out.println("Método: MA1");
+
+    }
+
+    public void MA2 () {
+
+        System.out.println("Método: MA2");
+
+    }
+
+}
+
+
+
---
+++ B.java
@@ -0,0 +1,49 @@
+public class B {
+
+    private int B1;
+    private float B2;
+
+    public B () {
+
+        B1 = 0;
+        B2 = 0;
+
+    }
+
+    public int getB1 () {
+
+        return B1;
+
+    }
+
+    public float getB2 () {
+
+        return B2;
+
+    }
+
+    public void setB1 (int mudarValor) {
+
+        B1 = mudarValor;
+
+    }
+
+    public void setB2 (float mudarValor) {
+
+        B2 = mudarValor;
+
+    }
+
+    public void MB1 () {
+
+        System.out.println("Método: MB1");
+
+    }
+
+    public void MB2 () {
+
+        System.out.println("Método: MB2");
+
+    }
+
+}
