d851f669cfe487db19c710142588fbcf8c399242
Scooby202 <erickscooby2021@gmail.com>
2026-04-17 19:00:58 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,48 @@
+public class D {
+    private int D1;
+    private float D2;
+
+    public D () {
+
+        D1 = 0;
+        D2 = 0;
+
+    }
+
+    public int getD1 () {
+
+        return D1;
+
+    }
+
+    public float getD2 () {
+
+        return D2;
+
+    }
+
+    public void setD1 (int mudarValor) {
+
+        D1 = mudarValor;
+
+    }
+
+    public void setD2 (float mudarValor) {
+
+        D2 = mudarValor;
+
+    }
+
+    public void MD1 () {
+
+        System.out.println("Método: MD1");
+
+    }
+
+    public void MD2 () {
+
+        System.out.println("Método: MD2");
+
+    }
+
+}
