f3d492f3e221294a7d69f70ea03e628fbc7702de
Scooby202 <erickscooby2021@gmail.com>
2026-04-17 17:20:57 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,49 @@
+public class C {
+
+    private String C1;
+    private int C2;
+
+    public C () {
+
+        C1 = "";
+        C2 = 0;
+
+    }
+
+    public String getC1 () {
+
+        return C1;
+
+    }
+
+    public int getC2 () {
+
+        return C2;
+
+    }
+
+    public void setC1 (String mudarValor) {
+
+        C1 = mudarValor;
+
+    }
+
+    public void setC2 (int mudarValor) {
+
+        C2 = mudarValor;
+
+    }
+
+    public void MC1 () {
+
+        System.out.println("Método: MC1");
+
+    }
+
+    public void MC2 () {
+
+        System.out.println("Método: MC2");
+
+    }
+
+}
